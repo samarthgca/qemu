01c97db42b0306db0d7f0a2cfd774f1a752076af
Samarth Gupta <samarthgca@gmail.com>
2026-07-20 16:38:11 -0700

    ROR

--- target/arc/translate.c
+++ target/arc/translate.c
@@ -492,6 +492,50 @@ static bool trans_ASR_S(DisasContext *dc, arg_asr_s *a)
     return true;
 }
 
+static bool trans_ROR(DisasContext *dc, arg_ror *a)
+{
+    tcg_gen_rotr_i32(cpu_regs[a->a], cpu_regs[a->b], cpu_regs[a->c]);
+    return true;
+}
+
+
+static bool trans_ROR_U6(DisasContext *dc, arg_ror_u6 *a)
+{
+    tcg_gen_rotri_i32(cpu_regs[a->a], cpu_regs[a->b], a->u);
+    return true;
+}
+
+
+static bool trans_ROR_S12(DisasContext *dc, arg_ror_s12 *a)
+{
+    tcg_gen_rotri_i32(cpu_regs[a->b], cpu_regs[a->b], a->s);
+    return true;
+}
+
+static bool trans_ROR_CC(DisasContext *dc, arg_ror_cc *a)
+{
+    if (a->q == 0) {
+        tcg_gen_rotr_i32(cpu_regs[a->b], cpu_regs[a->b], cpu_regs[a->c]);
+    } else if (a->q == 2) {
+        TCGv_i32 tmp = tcg_temp_new_i32();
+        tcg_gen_rotr_i32(tmp, cpu_regs[a->b], cpu_regs[a->c]);
+        tcg_gen_movcond_i32(TCG_COND_EQ, cpu_regs[a->b], cpu_zf, tcg_constant_i32(0), tmp, cpu_regs[a->b]);
+    }
+    return true;
+}
+
+static bool trans_ROR_CC_U6(DisasContext *dc, arg_ror_cc_u6 *a)
+{
+    if (a->q == 0) {
+        tcg_gen_rotri_i32(cpu_regs[a->b], cpu_regs[a->b], a->u);
+    } else if (a->q == 2) {
+        TCGv_i32 tmp = tcg_temp_new_i32();
+        tcg_gen_rotri_i32(tmp, cpu_regs[a->b], a->u);
+        tcg_gen_movcond_i32(TCG_COND_EQ, cpu_regs[a->b], cpu_zf, tcg_constant_i32(0), tmp, cpu_regs[a->b]);
+    }
+    return true;
+}
+
 static bool trans_CMP(DisasContext *dc, arg_cmp *a)
 {
     TCGv_i32 tmp = tcg_temp_new_i32();
